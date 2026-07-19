4671336aba41ab119bb9a598d0f9f55ce63f458d
trynachiket-collab <try.nachiket@gmail.com>
2026-07-19 21:01:37 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,53 +7,52 @@ export default function Home() {
       <div className="p-16 mx-auto max-w-3xl text-center">
         <Image
           src="/color/flowers/peony.png"
-          alt="anemone flower"
+          alt="Peony flower"
           width={100}
           height={100}
           className="object-cover mx-auto mb-6"
           priority
         />
+
         <Image
           src="/digibouquet.png"
-          alt="digibouquet"
+          alt="Nachi Bouquet"
           width={600}
           height={400}
           className="object-cover mx-auto"
           priority
         />
+
         <p className="my-6 text-sm md:mb-6 md:-mt-6">
-          beautiful flowers <br /> delivered digitally
+          A bouquet made with love <br /> that blooms forever 💐
         </p>
+
         <div className="flex flex-col justify-center items-center">
           <Link
             href="/bouquet?mode=color"
             className="text-sm px-8 py-4 bg-[#000000] text-[#F5F5DC] hover:bg-[#0A0000]/90 m-2"
           >
-            BUILD A BOUQUET
+            🌸 BUILD A BOUQUET
           </Link>
 
           <Link
             href="/bouquet?mode=mono"
             className="text-sm px-8 py-4 border border-black text-[#000000] hover:bg-[#F5F5AC]/90 m-2"
           >
-            BUILD IT IN BLACK AND WHITE
+            🖤 BUILD IN BLACK & WHITE
           </Link>
+
           <Link
             href="/garden"
             className="text-sm px-8 py-4 underline text-[#000000] m-2"
           >
-            VIEW GARDEN
+            🌼 VIEW GARDEN
           </Link>
         </div>
 
         <p className="mt-10 text-sm text-gray-500">
-          made by{" "}
-          <Link
-            href="https://x.com/pau_wee_"
-            className="mt-2 text-sm text-gray-500 underline"
-          >
-            @pau_wee_
-          </Link>
+          Made with ❤️ by{" "}
+          <span className="font-semibold text-black">Nachi</span>
         </p>
       </div>
     </div>
